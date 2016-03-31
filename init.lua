937fbf60c2ed69704b51456414635b4b1dd3ba6e
KwirkyJ <kwirkyj.smith0@gmail.com>
2016-03-31 11:20:33 -0500

    Preparing for github release; goodbye, mercurial

---
+++ init.lua
@@ -0,0 +1,231 @@
+-- Utility library to support useful operations on tables not present in the 
+-- standard library.
+-- 
+-- Author:  Jake Smith (KwirkyJ) <fallback1681@gmail.com>
+-- Date:    2016
+-- Version: 1.1
+-- License: MIT (X11) License
+--
+-- Special thanks to the authors of LuaUnit, the modification of which made
+-- some of the work here a repeat exercise. 
+-- (I wrote code there and then re-wrote it here for modularity.)
+
+
+
+local Buffer = require 'lua_stringbuffer' --'stringbuffer'
+
+
+
+local DEFAULT_DELTA = 1e-12
+
+local t = {_VERSION = "1.1",
+           _delta   = DEFAULT_DELTA,
+          }
+
+
+
+---moretables:getTolerance()
+-- Get the delta/tolerance used in tables.alike when none is supplied.
+-- @return number.
+local getTolerance = function(self)
+    return self._delta
+end
+t.getTolerance = getTolerance
+
+---moretables:setTolerance(delta)
+-- Set the delta/tolerance used in tables.alike when none is supplied.
+-- @param delta {Number} (default: DEFAULT_DELTA).
+-- @error iff delta is neither number nor nil.
+local setTolerance = function(self, delta)
+    delta = delta or DEFAULT_DELTA
+    assert(type(delta) == 'number', 'delta argument must be a number')
+    self._delta = delta
+end
+t.setTolerance = setTolerance
+
+
+
+---moretables.len(t)
+-- Get the number of elements in a table (shallow count).
+-- @param t The table.
+-- @return nil and failure message iff not a table;
+--         else the count of elements in the table.
+len = function(t)
+ if type(t) ~= 'table' then 
+        return nil, "Table expected, received "..type(t)
+    end
+    local count = 0
+    for _,_ in pairs(t) do
+        count = count + 1
+    end
+    return count
+end
+t.len = len
+
+---moretables.alike(t1, t2[, maxdelta] [, use_mt] [, loc])
+-- Check whether two tables are equivalent;
+-- Will accept non-tables; (searches nested tables).
+-- @param t1
+-- @param t2
+-- @param maxdelta Maximum difference between values, useful with
+--                 rounding errors (default DEFAULT_DELTA).
+-- @param use_mt   Boolean flag to use metatables (default false).
+-- @param loc      Internal string to place mismatched elements.
+-- @return True iff tables share same keys and respective values differ by 
+--         less than maxdelta; else false and failure message.tab1
+alike = function(t1, t2, maxdelta, use_mt, loc)
+    -- the code would be much more elegant if it was not as concerned with
+    -- providing error messages.
+    maxdelta = maxdelta or t._delta
+    local preface = ''
+    if type(t1) ~= type(t2) then 
+        preface = 'Differing types'
+        if loc then preface = 'Differing types at ' .. loc end
+        return false,string.format('%s: %s ~= %s', preface, type(t1), type(t2))
+    elseif type(t1) == 'number' then
+        if (math.max(t1,t2) - math.min(t1,t2) > maxdelta) then
+            if loc then
+                preface = 'First differing element at ' .. loc .. ': '
+            end
+            return false, string.format('%s(%s - %s) > %s',
+                          preface,
+                          tostring(math.max(t1,t2)),
+                          tostring(math.min(t1,t2)),
+                          maxdelta)
+        end
+    elseif type(t1) == 'table' then
+        if use_mt and (getmetatable(t1) or getmetatable(t2) )then
+            preface = 'Tables unequal'
+            if t1 == t2 then return true end
+            if loc then preface = 'Tables unequal at: ' .. loc end
+            return false, preface
+        end
+        local len1, len2 = len(t1), len(t2)
+        if len1 ~= len2 then
+            preface = 'Tables of differing length'
+            if loc then
+                preface = 'Tables of differing length at ' .. loc
+            end
+            return false, string.format('%s: %s ~= %s', preface, len1, len2)
+        end
+        
+        loc = loc or ""
+        local key
+        for k, v1 in pairs(t1) do
+            if type(k) == 'string' then 
+                key = string.format("%s['%s']", loc, k)
+            else
+                key = string.format("%s[%d]", loc, k)
+            end
+            local ok, err = alike(v1, t2[k], maxdelta, use_mt, key)
+            if not ok then return ok, err end
+        end
+    else -- same-type, neither tables nor numbers
+        if type(t1) == 'string' then t1 = "'"..t1.."'"end
+        if type(t2) == 'string' then t2 = "'"..t2.."'"end
+        if loc then preface = 'First differing element at '..loc..': ' end
+        if t1 ~= t2 then
+            return false, string.format('%s%s ~= %s', 
+                                        preface, tostring(t1), tostring(t2))
+        end
+    end
+    return true
+end
+t.alike = alike
+
+
+
+---Inner function for sorting a list by keys;
+-- assumption: only numbers and strings can be keys (values in orderedI);
+-- if same type, uses '<'
+-- else, numbers come first.
+local function _comp(a,b)
+    if type(a) == type(b) then
+        return a < b
+    elseif type(a) == 'number' then
+        return true
+    else 
+        return false
+    end
+end
+
+---moretables.getOrderedKeys(t[, comp][, filter])
+-- Get a list of keys in the table in the order specified by the comp function
+-- (default: alphabetic with sorted numeric keys first).
+-- @param t      {Table} Element from which to get keys.
+-- @param comp   {Function} Specifies key order;
+--               must accept two elements and return true if the first element
+--               is to come before the latter (Optional).
+-- @param filter {Function} Provides rules for which keys are to be retained;
+--               must accept one argument (string or number)
+--               must return true if the given key is to be added to the list
+--               (Optional).
+-- @error Raised iff <t> is not a function 
+--               or comp is neither function nor nil.
+-- @return {Table} List of keys in specified order.
+local getOrderedKeys = function(t, comp, filter)
+    comp = comp or _comp
+    filter = filter or function(_) return true end
+
+    assert(type(t) == 'table', "t must be a table; was ".. type(t))
+    assert(type(filter) == 'function', 'filter must be a function')
+    assert(type(comp) == 'function', 'comp must be a function')
+
+    local list, count = {}, 0
+
+    for k,_ in pairs(t) do
+        if filter(k) then
+            count = count + 1
+            list[count] = k
+        end
+    end
+    table.sort(list, comp)
+    return list
+end
+t.getOrderedKeys = getOrderedKeys
+
+
+
+---moretables.tostring(t[, indent])
+-- Convert the provided entity to a pretty string.
+-- @param t      Should be a table, but accepts anything.
+-- @param indent Number for the number of spaces to indent (default 0).
+-- @return Iff type(t) ~= 'table' then tostring(t);
+--         else, pretty-prints the table and any nested contents.
+toStr = function(t, indent)
+    if type(t) ~= 'table' then return tostring(t) end
+    
+    local sb, indices, key
+    sb = Buffer.new('\n') -- set buffer to add newlines after each addition
+    indices= getOrderedKeys(t)
+
+    if #indices == 0 then return '{}' end -- empty table
+    
+    indent = indent or 0
+    local ins, value = string.rep(' ', indent+2), nil
+    sb:add('{')
+    for i=1, #indices do
+        key = indices[i]
+        value = t[key]
+        if type(t[key]) == 'string' then
+            value = "'"..value.."'"
+        else
+            value = toStr(value, indent + 2)
+        end
+        if type(key) == 'string' then
+            key = "'" .. key .. "'"
+        else
+            key = tostring(key)
+        end
+        sb:add(string.format('%s[%s] = %s', ins, key, value))
+    end
+    sb:setSuffix() -- stop auto-adding newlines
+    sb:add(string.rep(' ', indent) .. '}')
+    return sb:getString()
+end
+t.tostring = toStr
+
+
+
+return t
+
---
+++ test/moretables.lua
@@ -0,0 +1,257 @@
+--Test suite for the moretables library
+--
+-- Author:  J. 'KwirkyJ' Smith <kwirkyj.smith0@gmail.com>
+-- Date:    2016
+-- Version: 1.0.0
+-- License: MIT (X11) License
+
+
+
+local tables = require 'moretables'
+
+local a, e -- actual, expected
+
+
+
+---- settings for module -----------------------------------------------------
+
+assert(tables:getTolerance() == 1e-12,
+       'default tolerance')
+assert(not pcall(tables.setTolerance, tables, '4'),
+       'setTolerance requires number')
+assert(not pcall(tables.setTolerance, tables, {}),
+       'setTolerance requires number')
+
+tables:setTolerance(1)
+assert(tables:getTolerance() == 1)
+
+tables:setTolerance()
+assert(tables:getTolerance() == 1e-12,
+       'setTolerance(nil) resets to default')
+
+
+
+---- tables.len --------------------------------------------------------------
+
+assert(tables.len({}) == 0,
+       'empty table')
+assert(tables.len({3, 2, 0.05, "blue", {}, nil, 8}) == 6, 
+       'nil is ignored')
+assert(tables.len({a=4, b={}, c="cerise"}) == 3,
+       'counts non-numeric indices')
+
+for _,v in ipairs{nil, 3, 'e', function () return nil end} do
+    local result, msg = tables.len(v)
+    assert(result == nil)
+    assert(msg == "Table expected, received " .. type(v),
+           'error condition when not a table')
+end
+
+
+
+---- tables.alike ------------------------------------------------------------
+
+-- non-tables works like '=='
+
+local ok, msg = tables.alike(-3, {})
+assert(ok == false,
+       'failure is a boolean-false value')
+assert(msg == "Differing types: number ~= table",
+       'number/table')
+
+ok, msg = tables.alike({a=2, b=1}, "oranges")
+assert(not ok)
+assert(msg == "Differing types: table ~= string",
+       'table/string')
+
+ok, msg = tables.alike(nil, {2, "glue"})
+assert(not ok)
+assert(msg == "Differing types: nil ~= table",
+       'nil/table')
+
+assert(tables.alike(nil, nil),
+       "nil is like nil")
+assert(tables.alike('s', "s"),
+       "like strings")
+assert(tables.alike(4, 4),
+       "like numbers")
+assert(tables.alike(4, 4.01, 0.1),
+       "delta param works here too")
+
+
+
+-- comparing tables
+
+assert(tables.alike({}, {}),
+       'empty tables are alike')
+assert(tables.alike({}, {}) == true,
+       'confirm that this returns a boolean-true value')
+assert(tables.alike({a=5, b=1.1}, {a=5, b=1.1}),
+       'alike tables are alike')
+assert(tables.alike({a="blue", b={{4, math.pi}, {{{k=1.2}}, nil}}},
+                    {a="blue", b={{4, math.pi}, {{{k=1.2}}, nil}}}),
+       'compares nested elements')
+assert(tables.alike({1 + 6e-13}, {1}),
+       'implicit tolerance for rounding errors') -- default 1e-12
+assert(not tables.alike({0.0000000000096}, {1}),
+       'difference is too large for default delta')
+assert(tables.alike({a=0,            b=1.0000001}, 
+                    {a=0.0000000034, b=1}, 
+                    1e-4), 
+       'third argument adjusts tolerance')
+assert(tables.alike({{1.04}}, {{1}}, 0.05),
+       'nested values "close enough"')
+
+
+local mt = {__eq = function() return true end} 
+local t1, t2 = {5}, {{{}}} -- definitely not equal
+assert(not tables.alike(t1, t2),
+       'assuredly different')
+setmetatable(t1, mt)
+setmetatable(t2, mt)
+assert(tables.alike(t1, t2, nil, true),
+       'use the metatable which says they are equal')
+
+
+local ok, msg = tables.alike(-3, {})
+assert(ok == false,
+       'failure is a boolean-false value')
+assert(msg == "Differing types: number ~= table")
+
+ok, msg = tables.alike({a=2, b=1}, "oranges")
+assert(not ok)
+assert(msg == "Differing types: table ~= string",
+       'table/string')
+
+ok, msg = tables.alike(nil, {2, "glue"})
+assert(not ok)
+assert(msg == "Differing types: nil ~= table",
+       'nil/table')
+
+ok, msg = tables.alike(3, 8)
+assert(not ok)
+assert(msg == "(8 - 3) > 1e-12",
+       'beyond delta')
+
+
+
+-- test error messages
+
+ok, msg = tables.alike({a=1, b=2}, {a=1}, 1e-9)
+assert(not ok)
+assert(msg == "Tables of differing length: 2 ~= 1")
+
+ok, msg = tables.alike({a=1, b={1,2}}, {a=1, b={1,2,3}})
+assert(not ok)
+assert(msg == "Tables of differing length at ['b']: 2 ~= 3",
+       msg)
+
+ok, msg = tables.alike({a=1, b=2}, {a="green", b=2}, 1e-9)
+assert(not ok)
+assert(msg == "Differing types at ['a']: number ~= string")
+
+ok, msg = tables.alike({a=0, b=1}, {b=1, c=2})
+assert(not ok)
+assert(msg == "Differing types at ['a']: number ~= nil")
+
+ok, msg = tables.alike({a=0}, {a=1})
+assert(not ok)
+assert(msg == "First differing element at ['a']: (1 - 0) > 1e-12")
+
+ok, msg = tables.alike({4, {['1'] = {a="blue"}}}, {4, {['1'] = {a="green"}}})
+assert(not ok)
+assert(msg == "First differing element at [2]['1']['a']: 'blue' ~= 'green'")
+
+ok, msg = tables.alike({a=0, b=1, c=1}, 
+                           {a=0, b=1, c=0.9999999}, 
+                           1e-12)
+assert(not ok)
+assert(msg == "First differing element at ['c']: (1 - 0.9999999) > 1e-12")
+
+t1, t2 = {a=5}, {[3] = true}
+local mt = {__eq = function(self) return self[1] ~= nil end}
+setmetatable(t1, mt)
+setmetatable(t2, mt)
+_,msg = tables.alike(t1, t2, nil, true)
+assert(msg == 'Tables unequal')
+
+--metatable will cause nested t=t1 to fail
+ok, msg = tables.alike({{4,{t=t1}, 3}}, {{4, {t=t2}, 3}}, nil, true)
+assert (not ok)
+assert(msg == "Tables unequal at: [1][2]['t']", msg)
+
+
+
+---- tables.getOrderedKeys ---------------------------------------------------
+
+local getseq = tables.getOrderedKeys
+local e = {{}, 
+           {1, 6}, 
+           {'Red', '_blue', 'apples'}, 
+           {-4, 1, 'kumquat'}
+          }
+local a = {{}, 
+           {[6]='orange', [1]=function() end},
+           {['_blue'] = true, Red = false, ['apples'] = {}},
+           {{}, [-4] = '', kumquat = 'pickelbarrel'}
+          }
+for i=1, 4 do
+    assert(tables.alike(getseq(a[i]), e[i]),
+           'unexpected mismatch at ' .. tostring(i))
+end
+
+
+e = {'oranges', 'bananas'}
+a = getseq({bananas     = true, 
+            blueberries = true,
+            oranges     = true,
+            tangerienes = true,
+           },
+           function(a,b) return a > b end,
+           function(s) return not s:find('i') end)
+assert(tables.alike(actual, expected),
+       'filter and comp at work')
+
+
+--TODO: test/demonstrate type-checking of parameters
+
+
+
+---- tables.tostring ---------------------------------------------------------
+
+local str = tables.tostring
+
+--non-tables return tostring of the thing
+assert(str() == 'nil')
+assert(str(5) == '5')
+assert(str('s') == 's')
+assert(str(str) == tostring(str),
+       'something like "function: 0x12345678910"')
+
+assert(str{} == '{}')
+
+t1 = {{{ ['b'] = {['1'] = 1,
+                  1, -- [1] = 1,
+                  [5]   = 'k',
+                  blue  = {},
+                  green = true,
+                 }
+     }}}
+assert(str(t1) == [=[{
+  [1] = {
+    [1] = {
+      ['b'] = {
+        [1] = 1
+        [5] = 'k'
+        ['1'] = 1
+        ['blue'] = {}
+        ['green'] = true
+      }
+    }
+  }
+}]=], str(t1))
+
+
+
+
+print("==== TEST MORETABLES PASSED ====")
